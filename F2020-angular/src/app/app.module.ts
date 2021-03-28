f0e6d1f5f059164aeb0e9046834b54b7f9a86235
AngelinaZhukovich <73842459+AngelinaZhukovich@users.noreply.github.com>
2021-03-28 15:58:24 +0300

    Add files via upload

---
+++ F2020-angular/src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+import { AppComponent } from './app.component';
+import { FormsModule }   from '@angular/forms';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { ItemDetailComponent } from './components/item-detail/item-detail.component';
+import { SearchComponent } from './components/search/search.component';
+import { DragScrollModule } from 'ngx-drag-scroll';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavigationComponent,
+    HomeComponent,
+    AboutComponent,
+    ItemDetailComponent,
+    SearchComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    DragScrollModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
